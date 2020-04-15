e26def6439ddfb77f2e2c9e1bdc627380529ee05
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-15 10:52:59 +0400

    Добавьте файлы проекта.

---
+++ Interface lesson/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_lesson
+{
+    class Program
+    {
+                     
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() { Name = "Василий", Age = 20 };
+            Student student2 = new Student() { Name = "Наталья", Age = 20 };
+
+            Student[] students = new Student[] { student1, student2 };
+
+            Array.Sort(students);
+        }
+        public struct Student: IComparable
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int CompareTo(object obj)
+            {
+                Student student = (Student)obj;
+                if (this.Age > student.Age) return 1;
+                if (this.Age > student.Age) return -1;
+                return 0;
+
+
+            }
+        }
+    }
+}
